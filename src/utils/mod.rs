c0e0ed1714e7eacde2e843ad1989cd5b1e1f4980
Naythaniel Williams <aretheregods@gmail.com>
2020-05-17 11:05:41 -0400

    Added case insensitive search matching in utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -9,22 +9,36 @@ pub struct Config {
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() == 2 {
-            return Err("Missing file argument")
+            return Err("Missing file argument");
         } else if args.len() == 1 {
-            return Err("Missing query and file arguments")
+            return Err("Missing query and file arguments");
         } else {
             let query = args[1].clone();
             let filename = args[2].clone();
 
-            Ok(Config { query, filename})
-        }   
+            Ok(Config { query, filename })
+        }
     }
+}
+
+pub fn search<'a>(query: &str, file_contents: &'a str) -> Vec<&'a str> {
+    let mut results = vec![];
+
+    for line in file_contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
