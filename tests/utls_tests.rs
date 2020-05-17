21b512442eacf5d8b4c9be33e7d9b63a8c5221ae
Naythaniel Williams <aretheregods@gmail.com>
2020-05-17 12:00:31 -0400

    Added sensensitve insensitive search tests

--- tests/utls_tests.rs
+++ tests/utls_tests.rs
@@ -1,14 +1,29 @@
 mod tests {
-    use minigrep::utils::{search};
+    use minigrep::utils::{search, search_case_insensitive};
 
     #[test]
-    fn one_result() {
-        let query = "something";
+    fn search_result() {
+        let query = "Thing";
         let contents = "\
         Rust,
         something, something, whatever
-        Pick a thing.";
+        Pick a Thing.";
 
-        assert_eq!(vec!["        something, something, whatever"], search(query, contents));
+        assert_eq!(
+            vec!["        Pick a Thing."],
+            search(query, contents));
+    }
+
+    #[test]
+    fn search_result_insensitive() {
+        let query = "thing";
+        let contents = "\
+        Rust,
+        something, something, whatever
+        Pick a Thing.";
+
+        assert_eq!(
+            vec!["        something, something, whatever", "        Pick a Thing."],
+            search_case_insensitive(query, contents));
     }
 }
